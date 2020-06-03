613c3dceb10934cb5bb73c5c6d622cb92f9710f3
Mukundmurarka <mukundmurarka1996@gmail.com>
2020-06-03 21:55:27 +0530

    fifth

--- src/View_Student.java
+++ src/View_Student.java
@@ -4,24 +4,14 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import net.proteanit.sql.DbUtils;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author MUKUND MURARKA
- */
 public class View_Student extends javax.swing.JFrame {
     public Connection con=null;
     public PreparedStatement pst=null;
-    /**
-     * Creates new form View_Student
-     */
+    
     public View_Student() {
         initComponents();
         try{
@@ -32,7 +22,7 @@ public class View_Student extends javax.swing.JFrame {
            
             ResultSet rs=pst.executeQuery();
             jTable1.setModel(DbUtils.resultSetToTableModel(rs));
-            //JOptionPane.showMessageDialog(null,"successfull ragistered");
+            
         }catch(Exception ece){
             JOptionPane.showMessageDialog(null,"unsuccessfull register");
         }
@@ -47,6 +37,8 @@ public class View_Student extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jdeletebtn = new javax.swing.JButton();
+        jbackbtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -70,6 +62,20 @@ public class View_Student extends javax.swing.JFrame {
         jTable1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         jScrollPane1.setViewportView(jTable1);
 
+        jdeletebtn.setText("delete");
+        jdeletebtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jdeletebtnActionPerformed(evt);
+            }
+        });
+
+        jbackbtn.setText("BACK");
+        jbackbtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbackbtnActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -78,17 +84,51 @@ public class View_Student extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 693, Short.MAX_VALUE)
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(85, 85, 85)
+                .addComponent(jdeletebtn, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(187, 187, 187)
+                .addComponent(jbackbtn, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(212, Short.MAX_VALUE))
+                .addGap(43, 43, 43)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jdeletebtn, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jbackbtn, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 186, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jdeletebtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jdeletebtnActionPerformed
+        // TODO add your handling code here:
+        //DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
+        int SelectedRowIndex=jTable1.getSelectedRow();
+        String cell= jTable1.getModel().getValueAt(SelectedRowIndex, 0).toString();
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/admin_detail?zeroDateTimeBehavior=convertToNull","root","");
+            String sql ="DELETE FROM `student_detail` WHERE fname = "+cell;
+            
+            pst = con.prepareStatement(sql);
+            
+            //model.removeRow(SelectedRowIndex);
+            pst.execute();
+            
+        }catch(Exception ex){
+           JOptionPane.showMessageDialog(null, "select a row ");
+        }
+    }//GEN-LAST:event_jdeletebtnActionPerformed
+
+    private void jbackbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbackbtnActionPerformed
+        // TODO add your handling code here:
+        new Admin_Section().setVisible(true);
+    }//GEN-LAST:event_jbackbtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -128,5 +168,7 @@ public class View_Student extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JButton jbackbtn;
+    private javax.swing.JButton jdeletebtn;
     // End of variables declaration//GEN-END:variables
 }
